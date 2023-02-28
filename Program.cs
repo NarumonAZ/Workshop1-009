9779fefb876c97718ffaba27bd97f2c278c08935
Pang <Narumonokanoto@gmail.com>
2023-02-28 20:36:01 +0700

    H4

--- Program.cs
+++ Program.cs
@@ -2,13 +2,17 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("Security Agency : ");
-            bool sa = bool.Parse(Console.ReadLine());
-
             Console.Write("Password : ");
             int num = int.Parse(Console.ReadLine());
-            Console.WriteLine();
-               
+            Console.WriteLine(); 
+
+           Console.Write("Codename : ");
+            string codename = Console.ReadLine();
+
+            
        }
 
     } 
+
+
+
