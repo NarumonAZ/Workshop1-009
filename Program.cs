0208715d6c72d683e97be3463599e00044e32a01
Pang <Narumonokanoto@gmail.com>
2023-02-28 21:25:25 +0700

    H5

--- Program.cs
+++ Program.cs
@@ -3,16 +3,39 @@
         static void Main(string[] args)
         {
             Console.Write("Password : ");
-            int num = int.Parse(Console.ReadLine());
-            Console.WriteLine(); 
-
+            int pass = int.Parse(Console.ReadLine());
+            
            Console.Write("Codename : ");
             string codename = Console.ReadLine();
 
-            
+            if (pass >= 0 && pass <= 999999) {
+                if (codename == "CIA" || codename == "FBI" || codename == "NSA") {
+                    switch (codename)
+                    {
+                        case "CIA" :
+                            if (pass >= 0 && pass <= 999999) {
+                                Console.WriteLine("True");
+                                } else {
+                                 Console.WriteLine("False"); }
+                            break;
+
+                        case "FBI" :
+                            if (pass >= 0 && pass <= 999999) {
+                                Console.WriteLine("True");
+                                } else {
+                                 Console.WriteLine("False"); }
+                            break;
+
+                        case "NSA" :
+                            if (pass >= 0 && pass <= 999999) {
+                                Console.WriteLine("True");
+                                } else {
+                                 Console.WriteLine("False"); }
+                        break;
+                    }
+                } else {
+                     Console.WriteLine("Password is incorrect"); }
+
        }
-
     } 
-
-
-
+}
